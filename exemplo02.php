a66171f551b8c7008f210082dbc58067b9a6f115
fabio <fabio@giter.com.br>
2022-02-03 17:21:11 -0300

    atualizacao curso 03-02

---
+++ exemplo02.php
@@ -0,0 +1,12 @@
+<?php
+
+$nome1 = "Fabio";
+
+$sobrenome = "Hanada";
+
+$nomeCompleto = $nome1. " " .$sobrenome;
+
+echo $nomeCompleto;
+
+
+?>
---
+++ exemplo03.php
@@ -0,0 +1,25 @@
+<?php
+
+$nome = "hcode";
+$site = 'www.uol.com.br';
+
+$ano = 1990;
+
+$salario = 55.99;
+$bloqueado = false;
+
+//////////////////////
+
+$frutas = array("abacaxi", "laranja", "manga");
+
+$nascimento = new DateTime();
+
+//var_dump($nascimento);
+
+$arquivo = fopen("exemplo03.php", "r");
+
+//var_dump($arquivo);
+
+$nulo = NULL;
+
+?>
