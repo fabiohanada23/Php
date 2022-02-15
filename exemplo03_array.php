d9c9ea6ce2c77829d57cd7060e8b40ef5d39bf8e
fabiohanada23 <61828534+fabiohanada23@users.noreply.github.com>
2022-02-15 12:07:59 -0300

    atualizacao curso 15-02

---
+++ exemplo03_array.php
@@ -0,0 +1,17 @@
+<?php
+
+$pessoas = array();
+
+array_push($pessoas, array(
+   'nome'=>'Joao',
+    'idade'=>20
+));
+
+array_push($pessoas, array(
+    'nome'=>'Fabio',
+    'idade'=>30
+));
+
+print_r($pessoas);
+
+?>
